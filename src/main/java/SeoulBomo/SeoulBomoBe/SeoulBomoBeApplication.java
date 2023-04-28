d596be1c4112ee44e787c8a63e25284245e061c4
류민아 <rmina1030@naver.com>
2023-04-28 23:16:16 +0900

    Initial commit

---
+++ src/main/java/SeoulBomo/SeoulBomoBe/SeoulBomoBeApplication.java
@@ -0,0 +1,13 @@
+package SeoulBomo.SeoulBomoBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulBomoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulBomoBeApplication.class, args);
+	}
+
+}
