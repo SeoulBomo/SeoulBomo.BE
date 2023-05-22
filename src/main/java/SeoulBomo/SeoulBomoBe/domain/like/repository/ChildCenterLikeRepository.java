911bc7d581d87ee3de103794e884fe1837709112
kyodkyo <dkekry@naver.com>
2023-05-22 15:51:48 +0900

    feat: 보육 정보, 센터 정보 좋아요 기능 구현

---
+++ src/main/java/SeoulBomo/SeoulBomoBe/domain/like/repository/ChildCenterLikeRepository.java
@@ -0,0 +1,20 @@
+package SeoulBomo.SeoulBomoBe.domain.like.repository;
+
+import SeoulBomo.SeoulBomoBe.domain.like.model.ChildCenterLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChildCenterLikeRepository extends JpaRepository<ChildCenterLike, Long> {
+    Optional<ChildCenterLike> findByAccount_IdAndChildCenterInfo_Id(Long userId, Long infoId);
+
+    @Modifying
+    @Query(value = "UPDATE child_center_like SET deleted = false WHERE child_center_like_id = :id", nativeQuery = true)
+    void updateDeletedFalse(Long id);
+
+    boolean existsByAccount_IdAndChildCenterInfo_Id(Long userId, Long infoId);
+}
