edcd99f0d82acbe5490f186992c016b75ee98caa
kyodkyo <dkekry@naver.com>
2023-05-23 14:32:04 +0900

    refactor: api url 변경

--- src/main/java/SeoulBomo/SeoulBomoBe/domain/childCenterInfo/controller/ChildCenterInfoController.java
+++ src/main/java/SeoulBomo/SeoulBomoBe/domain/childCenterInfo/controller/ChildCenterInfoController.java
@@ -1,17 +1,13 @@
 package SeoulBomo.SeoulBomoBe.domain.childCenterInfo.controller;
 
 import SeoulBomo.SeoulBomoBe.common.dto.PageResponse;
-import SeoulBomo.SeoulBomoBe.domain.childCenterInfo.model.ChildCenterInfo;
 import SeoulBomo.SeoulBomoBe.domain.childCenterInfo.service.ChildCenterInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,30 +15,31 @@ import static SeoulBomo.SeoulBomoBe.domain.childCareInfo.dto.ChildCareInfoDto.*;
 import static SeoulBomo.SeoulBomoBe.domain.childCenterInfo.dto.ChildCenterInfoDto.*;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class ChildCenterInfoController {
 
     @Autowired
     private ChildCenterInfoService childCenterInfoService;
 
-    @GetMapping("/api/v1/search/borough-center")
+    @GetMapping("search/borough-center")
     public ResponseEntity getBoroughCenter(@RequestParam("borough") String borough){
         List<Object> boroughList = childCenterInfoService.findBoroughList(borough);
         return new ResponseEntity(boroughList, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/search/keyword")
+    @GetMapping("search/keyword")
     public ResponseEntity getKeyWordCenter(@RequestParam("keyword") String keyword){
         List<Object> keywordList = childCenterInfoService.searchKeywordLists(keyword);
         return new ResponseEntity(keywordList, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/child-center/detail")
-    public ResponseEntity getChildCenterInfo(@RequestParam("id") Long id){
+    @GetMapping("child-center/{info-id}/detail")
+    public ResponseEntity getChildCenterInfo(@PathVariable("info-id") Long id){
         ChildCenterDetailResponse info = childCenterInfoService.findVerifiedCenterInfo(id);
         return new ResponseEntity(info, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/borough/center-list")
+    @GetMapping("borough/center-list")
     public ResponseEntity getBoroughCenterList(
             @PageableDefault Pageable pageable,
             @RequestBody ChildCenterBoroughListRequest childCenterBoroughListRequest
@@ -53,7 +50,7 @@ public class ChildCenterInfoController {
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/keyword/center-list")
+    @GetMapping("keyword/center-list")
     public ResponseEntity getKeyWordCenterList(
             @PageableDefault Pageable pageable,
             @RequestParam("keyword") String keyword
@@ -63,7 +60,7 @@ public class ChildCenterInfoController {
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/keyword/care-info-list")
+    @GetMapping("keyword/care-info-list")
     public ResponseEntity getKeyWordCareInfoList(
             @PageableDefault Pageable pageable,
             @RequestParam("keyword") String keyword
