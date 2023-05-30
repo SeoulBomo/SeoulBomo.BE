06cd37a525d7a6bc30d2897b4bb27dacbf69dcb9
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2023-05-30 21:39:23 +0900

    refactor: 키워드가 null인 경우 빈배열 반환

--- src/main/java/SeoulBomo/SeoulBomoBe/domain/childCenterInfo/service/ChildCenterInfoService.java
+++ src/main/java/SeoulBomo/SeoulBomoBe/domain/childCenterInfo/service/ChildCenterInfoService.java
@@ -44,10 +44,14 @@ public class ChildCenterInfoService {
     }
 
     public List<Object> searchKeywordLists(String keyword) {
+        List<Object> lists = new ArrayList<>();
+        if(keyword.isBlank() || keyword == null || keyword.isEmpty() || keyword.equals("")){
+            return lists;
+        }
+
         List<ChildCenterInfo> inCenterInfoList = childCenterInfoRepository.findTop3ByAddressORNameContaining(keyword);
         List<ChildCareInfo> inCareInfoList = childCareInfoRepository.findTop3ByAddressORNameContaining(keyword);
 
-        List<Object> lists = new ArrayList<>();
         lists.add(inCenterInfoList);
         lists.add(inCareInfoList);
 
